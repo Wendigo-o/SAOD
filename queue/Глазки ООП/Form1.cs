70a26479481c22e6665053d6378d1407871f39e9
Кретов Георгий Дмитриевич <KGD.21@free.uni-dubna.ru>
2022-10-08 15:11:41 +0300

    queue 1

---
+++ queue/Глазки ООП/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Глазки_ООП
+{
+    public partial class FORMA : Form
+    {
+        public FORMA()
+        {
+            InitializeComponent();
+        }
+
+        Queue<int> mystack = new Queue<int>(5);
+
+
+        void richtextbox()
+        {
+            richTextBox.Clear();
+            int[] mas = mystack.List();           
+            
+            for (int i = 0; i < mas.Length; i++)
+            {
+                richTextBox.Text += mas[i].ToString() + "\n";
+            }
+        }
+
+        private void push_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                mystack.Push(int.Parse(pushBox.Text));
+                richtextbox();
+                errorBox.Clear();
+            }
+            catch(Exception ex)
+            {
+                errorBox.Text = $"{ex.Message}"; 
+            }
+            topBox.Clear();
+            popBox.Clear();
+            pushBox.Clear();
+            pushBox.Focus();
+
+        }
+
+        private void pop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                popBox.Text = (mystack.Pop()).ToString();
+                richtextbox();
+                errorBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                errorBox.Text = $"{ex.Message}";
+            }
+            topBox.Clear();
+
+
+
+        }
+
+        private void top_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                topBox.Text = (mystack.Top()).ToString();
+                errorBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                errorBox.Text = $"{ex.Message}";
+            }
+            popBox.Clear();
+        }
+
+    }
+}
---
+++ queue/Глазки ООП/Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Глазки_ООП
+{
+
+    internal class Queue<T>
+    {
+        T[] mas; 
+        int head = 0; 
+        int tail = 0;
+        int count = 0;
+        public Queue(int capacity)
+        {
+            mas = new T[capacity];
+        }
+
+        public void Push(T push)
+        {
+            if (count < mas.Length)
+            {
+                mas[tail] = push;
+                if (tail == mas.Length - 1) { tail = 0; }
+                else { tail++; }
+                count++;
+            }
+            else
+            {
+                throw new MyException("Очередь переполнена");
+            }
+        }
+
+        public T Pop()
+        {
+            if (count != 0)
+            {
+                T top = mas[head];                             
+                if ( head == mas.Length - 1) { head = 0; }
+                else { head++; }                             
+                count--;
+                return top;
+            }
+            else
+            {
+                throw new MyException("Очередь пуста");
+            }
+        }
+
+        public T Top()
+        {
+            if (count != 0) return mas[head];
+            else throw new MyException("Очередь пуста");
+        }
+
+        public T[] List()
+        {
+            T[] mas_c = new T[count];
+            int count_mas = head;
+            for (int i = 0; i < count; i++)
+            {
+                if (count_mas >= mas.Length) { count_mas = 0; }
+                mas_c[i] = mas[count_mas];
+                count_mas++;
+            }
+                return mas_c;           
+        }
+    }
+}
