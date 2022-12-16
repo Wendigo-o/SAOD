720e5ff629e37403ab05724d593ccabd916f12c5
NechaevaAR <112623065+Wendigo-o@users.noreply.github.com>
2022-12-16 22:43:19 +0300

    opz
    Добавлен opz

---
+++ opz v4/OPZ/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyOPZ myopz = new MyOPZ();
+
+            Console.WriteLine("Введите выражение");
+
+            //try
+            //{
+            //    string stroka = Console.ReadLine();
+            //    string otvet = myopz.Opz(stroka);
+
+            //    Console.WriteLine(otvet);
+            //}
+            //catch (Exception le)
+            //{
+            //    Console.WriteLine(le.Message);
+            //}
+
+            string stroka = Console.ReadLine();
+            string otvet = myopz.Opz(stroka);
+
+            Console.WriteLine(otvet);
+
+            Console.ReadLine();
+
+        }
+    }
+}
